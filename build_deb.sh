e488a394d6ffc4d31eda13cad07f6959f31053ce
jan0 <jan0@free.fr>
2011-08-14 12:01:51 +0200

    add optimo's changes to cydia package

--- build_deb.sh
+++ build_deb.sh
@@ -5,7 +5,7 @@ SSHPORT=2222
 mkdir -p cydia/usr/lib
 
 cp isslfix.dylib cydia/usr/lib
-cp extrainst_ prerm cydia/DEBIAN
+cp postinst extrainst_ prerm cydia/DEBIAN
 
 ssh -p $SSHPORT root@$IPHONE_HOST 'rm -rf /var/root/cydia'
 scp -P $SSHPORT -r cydia root@$IPHONE_HOST:/var/root
